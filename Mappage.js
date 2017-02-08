709cc2b5ac199a394288f5ce5b8a3b4429e80b26
seanoneill505 <oneill505@gmail.com>
2017-02-08 16:38:15 -0700

    Update Mappage.js

--- Mappage.js
+++ Mappage.js
@@ -1,9 +1,9 @@
 function initialize() {
-	var classroom = new google.maps.LatLng(35.084280,-106.624073)
+	var classroom = new google.maps.LatLng(35.1177076,-106.4869905)
 	var mapOptions = {
-		zoom: 8,
+		zoom: 14,
 		center: classroom,
-		mapTypeId: google.maps.MapTypeId.SATELLITE
+		mapTypeId: google.maps.MapTypeId.TERRAIN
 	};
 	var map = new google.maps.Map(
 		document.getElementById("map_canvas"),
