f995faffc2e41cc9c43971d165155d9d90dee1bc
seanoneill505 <oneill505@gmail.com>
2017-02-14 15:03:19 -0700

    Update mapjs1.js

--- js/mapjs1.js
+++ js/mapjs1.js
@@ -1,1 +1,11 @@
-holder
+function initialize() {
+	var classroom = new google.maps.LatLng(35.1177076,-106.4869905)
+	var mapOptions = {
+		zoom: 12,
+		center: classroom,
+		mapTypeId: google.maps.MapTypeId.TERRAIN
+	};
+	var map = new google.maps.Map(
+		document.getElementById("map_canvas"),
+		mapOptions);
+}
