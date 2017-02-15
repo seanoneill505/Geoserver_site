ce875c8e77992b06a6f03d8ef7546a51868be348
seanoneill505 <oneill505@gmail.com>
2017-02-15 16:55:54 -0700

    Update mapjs2.js

--- js/mapjs2.js
+++ js/mapjs2.js
@@ -1,6 +1,6 @@
 function initialize() {
 	var foothills = new google.maps.LatLng(35.1177076,-106.4869905)
-  	Var pinotrailhead = new google.maps.LatLng(35.165335,-106.456333,127)
+  	var pinotrailhead = new google.maps.LatLng(35.165335,-106.456333,127)
 	var mapOptions = {
 		zoom: 12,
 		center: foothills,
