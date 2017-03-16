e382d97f816fd1b2cea9c35c91344a1247c68f0f
seanoneill505 <oneill505@gmail.com>
2017-03-16 13:22:59 -0600

    Update mapjs3.js

--- js/mapjs3.js
+++ js/mapjs3.js
@@ -1,8 +1,8 @@
 function initialize() {
-	var classroom = new google.maps.LatLng(36.060574,-107.961692)
+	var center = new google.maps.LatLng(36.060574,-107.961692)
 	var mapOptions = {
 		zoom: 18,
-		center: classroom,
+		center: center,
 		mapTypeId: google.maps.MapTypeId.sattelite
 	};
 	var map = new google.maps.Map(
