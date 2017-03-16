b6ac5e4033ac149cdae183e45f091c783d3c7aff
seanoneill505 <oneill505@gmail.com>
2017-03-16 13:21:55 -0600

    Update mapjs3.js

--- js/mapjs3.js
+++ js/mapjs3.js
@@ -1,1 +1,11 @@
-
+function initialize() {
+	var classroom = new google.maps.LatLng(36.060574,-107.961692)
+	var mapOptions = {
+		zoom: 18,
+		center: classroom,
+		mapTypeId: google.maps.MapTypeId.sattelite
+	};
+	var map = new google.maps.Map(
+		document.getElementById("map_canvas"),
+		mapOptions);
+}
