714daf44dba5006d407663835412da25e7613c1d
seanoneill505 <oneill505@gmail.com>
2017-03-28 09:41:51 -0600

    Create OL1.js

---
+++ js/OL1.js
@@ -0,0 +1,12 @@
+var myMap = new ol.Map({
+ target: 'map',
+ layers: [
+ new ol.layer.Tile({
+ source: new ol.source.OSM()
+})
+ ],
+ view: new ol.View({
+ center: ol.proj.fromLonLat([-106.624083,35.08427]),
+ zoom: 18
+ })
+});
