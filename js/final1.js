7d37ac9127cc517ddf72f48b17337edfeec2c423
seanoneill505 <oneill505@gmail.com>
2017-05-08 11:46:58 -0600

    Update final1.js

--- js/final1.js
+++ js/final1.js
@@ -1,4 +1,3 @@
-
 var projection = ol.proj.get('EPSG:2258');
 
 var Layer_Stamen_terrain = new ol.layer.Group({
@@ -10,31 +9,17 @@ var Layer_Stamen_terrain = new ol.layer.Group({
 });
 
 
-var basemap_tiled = new ol.layer.Tile({
-	source: new ol.source.TileWMS({
-	url: 'https://basemap.nationalmap.gov/arcgis/services/USGSTopo/MapServer/WmsServer?',
-	  params: {
-		LAYERS: 0,
-		FORMAT: 'image/png',
-		TRANSPARENT: true
-	  },
-	  attributions: [
-	    new ol.Attribution({
-		  html: 'Data provided by the <a href="http://basemap.nationalmap.gov">National Map</a>.'
-		})
-	  ]
-	})
-})
 
 
 
 
-var USFSNM = new ol.layer.Tile({
+
+var SO35106A6 = new ol.layer.Tile({
 	source: new ol.source.TileWMS({
 		attributions: new ol.Attribution({
 			html: '4'
 		}),
-		params: {'LAYERS':'ws_ronbeel:USFS_admin_bndR3'},
+		params: {'LAYERS':'ws_ronbeel:SO_35106-A6'},
 		url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
 		serverType: 'geoserver',
 		projection: projection,
@@ -45,12 +30,12 @@ var USFSNM = new ol.layer.Tile({
 
 })
 
-var NMwilderness3 = new ol.layer.Tile({
+var SO35106A5 = new ol.layer.Tile({
 	source: new ol.source.TileWMS({
 		attributions: new ol.Attribution({
 			html: '12'
 		}),
-		params: {'LAYERS':'ws_ronbeel:nlcs_wilderness_areas_2014'},
+		params: {'LAYERS':'	ws_ronbeel:SO_35106-A5'},
 		url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
 		serverType: 'geoserver',
 		projection: projection,
@@ -60,12 +45,12 @@ var NMwilderness3 = new ol.layer.Tile({
 	}),
 })
 
-var NMwilderness = new ol.layer.Tile({
+var SO35106A6CONT = new ol.layer.Tile({
 	source: new ol.source.TileWMS({
 		attributions: new ol.Attribution({
 			html: '7'
 		}),
-		params: {'LAYERS':'ws_ronbeel:USFSRegion3Wilderness'},
+		params: {'LAYERS':'ws_ronbeel:SO_35106-A6_CONT'},
 		url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
 		serverType: 'geoserver',
 		projection: projection
@@ -74,12 +59,12 @@ var NMwilderness = new ol.layer.Tile({
 	
 })
 
-var irasda = new ol.layer.Tile({
+var SO35106A5CONT = new ol.layer.Tile({
 	source: new ol.source.TileWMS({
 		attributions: new ol.Attribution({
 			html: '7'
 		}),
-		params: {'LAYERS':'ws_ronbeel:irasda_nm_dd'},
+		params: {'LAYERS':'	ws_ronbeel:SO_35106-A5_CONT'},
 		url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
 		serverType: 'geoserver',
 		projection: projection
@@ -88,12 +73,12 @@ var irasda = new ol.layer.Tile({
 	
 })
 
-var cont_divide = new ol.layer.Image({
+var SOtgr2006sebernlka = new ol.layer.Image({
 	source: new ol.source.ImageWMS({
 		attributions: new ol.Attribution({
 			html: 'State Boundary Restructured - USGS, National Atlas Release 5-14-12'
 		}),
-		params: {'LAYERS':'ws_ronbeel:nm_cont_div'},
+		params: {'LAYERS':'	ws_ronbeel:SO_tgr2006se_bern_lka'},
 		url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
 		serverType: 'geoserver'
 	})
@@ -106,10 +91,10 @@ var cont_divide = new ol.layer.Image({
 
 var map = new ol.Map({
   target: 'map_canvas',
-  layers: [ Layer_Stamen_terrain, USFSNM, irasda, NMwilderness, cont_divide,  NMwilderness3],
+  layers: [ Layer_Stamen_terrain, SO35106A5, SO35106A6, SO35106A5CONT, SO35106A6CONT, SOtgr2006sebernlka  ],
   view: new ol.View({
-    center: ol.proj.fromLonLat([-105.918167,34.593986]), 
-    zoom: 7
+    center: ol.proj.fromLonLat([-106.6298957,35.0754534]), 
+    zoom: 12
 	
   })
 });
